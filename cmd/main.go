8cbf683b1d48b1799ec043038956e8106855b814
jpdavis-prof <jason.davis@hpe.com>
2023-10-19 08:56:05 -0600

    initial

---
+++ cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"discovery/internal/routes"
+	"discovery/internal/utils"
+
+	"github.com/gin-gonic/gin"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Read yaml config
+	config := viper.New()
+	config.SetConfigType("yaml")
+	config.SetConfigName("discovery-server")
+	config.AddConfigPath("./configs")
+	err := config.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	config.WatchConfig()
+
+	// Parse cli flags
+	flag.StringP("bind", "b", ":8080", "Bind address")
+	flag.StringP("mongoserver", "s", "", "Mongo ip:port")
+	flag.StringP("database", "d", "", "Mongo database to use")
+	flag.StringP("collection", "c", "", "Mongo collection to use")
+	flag.Parse()
+	config.BindPFlags(flag.CommandLine)
+
+	// Set up variables from config/flags
+	bind := config.GetString("bind")
+	mongoserver := config.GetString("mongoserver")
+	database := config.GetString("database")
+	collection := config.GetString("collection")
+
+	// Connect to database
+	dbClient := utils.OpenDBconn(mongoserver)
+
+	// Exit cleanly on SIGINT / Ctrl-c
+	go func() {
+		sigchan := make(chan os.Signal)
+		signal.Notify(sigchan, os.Interrupt)
+		<-sigchan
+		log.Println("Received SIGINT. Quitting...")
+		utils.CloseDBconn(dbClient)
+		os.Exit(0)
+	}()
+
+	router := gin.Default()
+	routes.Routes(router, bind, dbClient, database, collection)
+
+	utils.CloseDBconn(dbClient)
+}
---
+++ internal/routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"discovery/internal/utils"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Routes(router *gin.Engine, bind string, dbClient *mongo.Client, database string, collection string) {
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"message": "Route not found"})
+	})
+
+	router.GET("/", utils.GetMachines(dbClient, database, collection))
+	router.GET("/:id", utils.GetMachineByID(dbClient, database, collection))
+	router.POST("/", utils.PostMachine(dbClient, database, collection))
+	router.Run(bind)
+}
